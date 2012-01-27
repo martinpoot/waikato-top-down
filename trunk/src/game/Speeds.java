6dec43762868b63a6e054fdd030ebbc8ff08a0b9
deaker.c@gmail.com <deaker.c@gmail.com@0b6dfc5f-5c00-bcef-35e8-35f1d33b32dc>
2012-01-27 23:10:18 +0000

    Updated references to sprites.

--- trunk/src/game/Speeds.java
+++ trunk/src/game/Speeds.java
@@ -2,7 +2,7 @@ package game;
 
 public class Speeds {
 	
-	public final static float background = 15.0f;
+	public final static float background = 45.0f;
 	public static final float playerspeed = 400.0f;
 	public final static float turretFireSpeed = 3.0f;
 
