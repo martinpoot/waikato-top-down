53652b025093c0c034883525749cdd41f193d7d4
andreas.loow@gmail.com <andreas.loow@gmail.com@0b6dfc5f-5c00-bcef-35e8-35f1d33b32dc>
2012-01-27 20:35:53 +0000

    set up the libraries properly and created the basic game engine, got the game running.

--- trunk/src/game/GameEngine.java
+++ trunk/src/game/GameEngine.java
@@ -1,5 +1,44 @@
 package game;
 
-public class GameEngine {
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
 
+public class GameEngine extends BasicGame{
+
+	public GameEngine(String title) {
+		super(title);
+		
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void render(GameContainer container, Graphics g)
+			throws SlickException {
+		g.drawString("Hello world", 0, 100);
+		
+	}
+
+	@Override
+	public void init(GameContainer container) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(GameContainer container, int delta)
+			throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public static void main(String[] args) throws SlickException {
+		         AppGameContainer app = 
+					new AppGameContainer(new GameEngine("Top down ghost zombie shooter"),800,600,false);
+		         app.setVSync(true);
+		         app.start();
+	}
 }
