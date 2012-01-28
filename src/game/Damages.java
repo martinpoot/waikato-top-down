207a1390221d1812d55d2f079a7322c0f2314304
deaker.c@gmail.com <deaker.c@gmail.com@0b6dfc5f-5c00-bcef-35e8-35f1d33b32dc>
2012-01-28 20:47:13 +0000

    Player takes 10 * turret damage when colliding with turrets.

--- src/game/Damages.java
+++ src/game/Damages.java
@@ -2,14 +2,21 @@ package game;
 
 public class Damages {
 
+	// Damage
 	public static final float playerDamage = 5.0f;
 	public static final float ghostDamage = playerDamage / 2;
 	public static final float turretDamage = 5.0f;
+	// Collision damage = this * actor damage
+	public static final float collisionModifier = 10.0f;
+	
+	// Health
 	public static final int turretHealth = 10;
 	public static final int playerHealth = 300;
 	
 	// Controls how quickly randomly generated turrets are added (per round)
-	public static int turretIncreaseRate	= 10;
+	public static int turretIncreaseRate = 10;
 	public static int initialTurretCount = 30;
+	
+	public static boolean turretCollision = true;
 	
 }
