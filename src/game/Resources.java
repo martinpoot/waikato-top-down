5b42239541e6009ad421fc35f87bb32b0d3679dc
andreas.loow@gmail.com <andreas.loow@gmail.com@0b6dfc5f-5c00-bcef-35e8-35f1d33b32dc>
2012-01-28 22:05:34 +0000

    added recorded sounds

--- src/game/Resources.java
+++ src/game/Resources.java
@@ -5,13 +5,20 @@ import java.util.Hashtable;
 
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.Sound;
 
 public class Resources {
 	
+
+	private static Hashtable<String,Image> sprites = new Hashtable<String,Image>();
+	private static Hashtable<String,Sound> sounds = new Hashtable<String,Sound>();
+	
+	public final static String bullet1 = "res/bullet.png";
+
 	public final static String playerBullet = "res/bullet-purple.png";
 	public final static String ghostBullet = "res/bullet-yellow.png";
 	public final static String turretBullet = "res/bullet-green.png";
-	
+
 	public final static String turretShootingDown = "res/turret-shooting-down.png";
 	public final static String turretShootingUp = "res/turret-shooting-up.png";
 	public final static String turretShootingRight = "res/turret-shooting-right.png";
@@ -22,14 +29,19 @@ public class Resources {
 	public static final String statusBackground = "res/status-background.png";
 	public static final String statusSlider = "res/status-slider.png";
 	public static final String splashImage = "res/splash-image.png";
-	
-	public static final String soundTurretHit = "res/music/turret-hit.ogg";
-	public static final String soundTurretExplosion = "res/music/explosion-sm.ogg";
-	public static final String soundPlayerExplosion = "res/music/player-dies.ogg";
-	public static final String soundPlayerShoot = "res/music/player-shoot.ogg";
-	
 
-	private static Hashtable<String,Image> sprites = new Hashtable<String,Image>();
+	
+	
+	public static final String soundTurretHit = "res/music/recorded/popp.ogg";//"res/music/turret-hit.ogg";
+	public static final String soundTurretExplosion = "res/music/recorded/bang.ogg";//"res/music/explosion-sm.ogg";
+	
+	public static final String soundPlayerExplosion = "res/music/recorded/nooo.ogg";//"res/music/player-dies.ogg";
+	public static final String soundPlayerShoot = "res/music/recorded/pew.ogg"; //"res/music/player-shoot.ogg";
+	
+	public static final String playerCollision = "res/music/recorded/ouch.ogg";
+	
+	
+	
 	
 	public static Image getSprite(String spriteResource) throws SlickException {
 		if(!sprites.contains(spriteResource)) {
@@ -37,4 +49,14 @@ public class Resources {
 		}
 		return sprites.get(spriteResource).copy();
 	}
+	
+		
+	public static Sound getSound(String soundResource) throws SlickException {
+		if(!sounds.contains(soundResource)) {
+			sounds.put(soundResource,new Sound(soundResource));
+		}
+		return sounds.get(soundResource);
+	}
+	
+	
 }
