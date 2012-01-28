b6bf2bf82460de3b3325afd8db76441a8c19cdef
deaker.c@gmail.com <deaker.c@gmail.com@0b6dfc5f-5c00-bcef-35e8-35f1d33b32dc>
2012-01-28 05:50:05 +0000

    Player ghosts do damage.
    Player ghost damage = half of player damage (set in Damages)
    
    Some game speed/player movement speed tweaking.

--- src/game/Speeds.java
+++ src/game/Speeds.java
@@ -2,12 +2,12 @@ package game;
 
 public class Speeds {
 	
-	public static float boost = 4.0f;
+	public static float boost = 1.5f;
 
 	public static final float playerBulletSpeed = 500.0f * boost;
 	
 	public final static float background = 45.0f * boost;
-	public static final float playerspeed = 400.0f * boost;
+	public static final float playerspeed = 300.0f * boost;
 	public final static float turretFireSpeed = 0.5f * boost;
 	public final static float bulletSpeed = 200.0f * boost;
 	public final static float playerFireSpeed = 3.0f * boost;
