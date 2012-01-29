8b83a3e553168677ce28e1edeb0bd3c10b313113
andreas.loow@gmail.com <andreas.loow@gmail.com@0b6dfc5f-5c00-bcef-35e8-35f1d33b32dc>
2012-01-29 01:37:46 +0000

    damages adjusted by Marting

--- src/game/Damages.java
+++ src/game/Damages.java
@@ -3,19 +3,15 @@ package game;
 public class Damages {
 
 	// Damage
-	public static final float playerDamage = 10.0f;
+	public static final float playerDamage = 5.0f;
 	public static final float ghostDamage = playerDamage;
-	public static final float turretDamage = 5.0f;
+	public static final float turretDamage = 2.0f;
 	// Collision damage = this * actor damage
 	public static final float collisionModifier = 10.0f;
 	
 	// Health
-	public static final int turretHealth = 20;
-	public static final int playerHealth = 300;
-	
-	// Controls how quickly randomly generated turrets are added (per round)
-	public static int turretIncreaseRate = 10;
-	public static int initialTurretCount = 30;
+	public static final int turretHealth = 10;
+	public static final int playerHealth = 200;
 	
 	public static boolean turretCollision = true;
 	
